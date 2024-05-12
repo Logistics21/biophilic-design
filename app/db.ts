39382f8f0205a972463ec01ec8c2e18faba29b11
David Halsey <dhalsey212@gmail.com>
2024-05-12 00:31:50 -0400

    Update createUser; Import UsersTable

--- app/db.ts
+++ app/db.ts
@@ -1,28 +1,48 @@
+import * as schema from 'drizzle/schema'
 import { drizzle } from 'drizzle-orm/postgres-js';
-import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+// import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 import { eq } from 'drizzle-orm';
 import postgres from 'postgres';
 import { genSaltSync, hashSync } from 'bcrypt-ts';
+import { UsersTable } from 'drizzle/schema';
 
 // Optionally, if not using email/pass login, you can
 // use the Drizzle adapter for Auth.js / NextAuth
 // https://authjs.dev/reference/adapter/drizzle
-let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
-let db = drizzle(client);
 
-let users = pgTable('User', {
-  id: serial('id').primaryKey(),
-  email: varchar('email', { length: 64 }),
-  password: varchar('password', { length: 64 }),
-});
-
-export async function getUser(email: string) {
-  return await db.select().from(users).where(eq(users.email, email));
+interface NewUserFormData {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
 }
 
-export async function createUser(email: string, password: string) {
-  let salt = genSaltSync(10);
-  let hash = hashSync(password, salt);
+const client = postgres(`${process.env.POSTGRES_URL!}`, { max: 1 });
+const db = drizzle(client, { schema })
 
-  return await db.insert(users).values({ email, password: hash });
+// const users = pgTable('users', {
+//   id: serial('id').primaryKey(),
+//   email: varchar('email', { length: 64 }),
+//   password: varchar('password', { length: 64 }),
+// });
+
+export async function getUser(email: string) {
+  return await db.select().from(UsersTable).where(eq(UsersTable.email, email));
+}
+
+export async function createUser(newUserFormData: NewUserFormData) {
+  const { email, password, firstName, lastName } = newUserFormData;
+  try {
+    let salt = genSaltSync(10);
+    let hash = hashSync(password, salt);
+  
+    return await db.insert(UsersTable).values({
+      email,
+      password: hash,
+      firstName,
+      lastName,
+    });
+  } catch (error) {
+    debugger
+  }
 }
