eddb48058622ad23232e98eb472933c77f61c792
David Halsey <dhalsey212@gmail.com>
2024-05-12 00:06:17 -0400

    Add drizzle config, database env

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { loadEnvConfig } from '@next/env';
+import { defineConfig } from 'drizzle-kit';
+ 
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+export default defineConfig({
+  schema: './drizzle/schema.ts',
+  out: "./drizzle",
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.POSTGRES_URL!,
+  },
+});
