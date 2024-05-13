b5f3d86f58c243e988676e71780087b028f20a59
David Halsey <dhalsey212@gmail.com>
2024-05-13 11:22:16 -0400

    Add user name to topnav

--- app/_components/topnav.tsx
+++ app/_components/topnav.tsx
@@ -1,6 +1,14 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
-export function TopNav() {
+export async function TopNav() {
+    const user = await currentUser()
+    let firstName;
+
+    if (user) {
+      firstName = user.firstName;
+    }
+
     return (
         <nav className="flex w-full items-center justify-end border-b p-4 text-xl font-semibold bg-black">
             <div className="flex flex-row items-center gap-4 text-white">
@@ -8,6 +16,7 @@ export function TopNav() {
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          {firstName}
           <UserButton />
         </SignedIn>
       </div>
