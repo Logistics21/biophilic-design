a6e0e5cdedab46700933417aa6295dc85afd42d4
David Halsey <dhalsey212@gmail.com>
2024-05-12 23:31:46 -0400

    Add back direct features to report form fields

--- app/create-report/page.tsx
+++ app/create-report/page.tsx
@@ -32,7 +32,7 @@ export default function CreateReportPage() {
   }
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
+    <div className="flex h-fit w-screen items-center justify-center bg-gray-50 py-5">
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-left justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-left sm:px-16">
           <h1 className="text-xl font-semibold">
