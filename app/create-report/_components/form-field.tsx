e0f9a8c2188ffdcfbda00355122d3c92ceead132
David Halsey <dhalsey212@gmail.com>
2024-06-02 14:42:40 -0400

    Format form field

--- app/create-report/_components/form-field.tsx
+++ app/create-report/_components/form-field.tsx
@@ -16,32 +16,31 @@ export const Field = ({
     return (
       <div style={{ paddingTop: '20px' }}>
         <label
-        htmlFor={formFieldId}
-        className="block text-xs text-gray-600 uppercase"
+          htmlFor={formFieldId}
+          className="block text-xs text-gray-600 uppercase"
         >
-        {`${fieldName}: ${fieldText}`}
+          {`${fieldName}: ${fieldText}`}
         </label>
         <input
-        type="range"
-        id={formFieldId}
-        name={formFieldId}
-        min={0}
-        max={3}
-        defaultValue={initialValueScore || 0}
-        required
-        disabled={disabled}
-        list="values"
-        style={{
-            width: '300px',
-        }}
+          type="range"
+          id={formFieldId}
+          name={formFieldId}
+          min={0}
+          max={3}
+          defaultValue={initialValueScore || 0}
+          required
+          disabled={disabled}
+          list="values"
+          style={{ width: '300px' }}
         />
-        <datalist id="values" style={{
-        display: 'flex',
-        // flexDirection: 'column',
-        justifyContent: 'space-between',
-        // writingMode: 'vertical-lr',
-        width: '300px'
-        }}>
+        <datalist
+          id="values"
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '300px'
+          }}
+        >
         <option value="0" label="0"></option>
         <option value="1" label="1"></option>
         <option value="2" label="2"></option>
