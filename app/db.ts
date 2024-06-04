520076069be1f21bc14d6c05cedda177160a5316
David Halsey <dhalsey212@gmail.com>
2024-06-04 09:49:18 -0400

    Remove clerkUserId, unused fields and comments

--- app/db.ts
+++ app/db.ts
@@ -1,25 +1,17 @@
 import * as schema from 'drizzle/schema'
 import { drizzle } from 'drizzle-orm/postgres-js';
-// import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 import { eq, and } from 'drizzle-orm';
 import postgres from 'postgres';
-import { genSaltSync, hashSync } from 'bcrypt-ts';
 import { UsersTable, ReportsTable } from 'drizzle/schema';
-
-// Optionally, if not using email/pass login, you can
-// use the Drizzle adapter for Auth.js / NextAuth
-// https://authjs.dev/reference/adapter/drizzle
 import { currentUser } from '@clerk/nextjs/server';
 
 interface NewUserFormData {
   email: string;
-  password: string;
   firstName: string;
   lastName: string;
   clerkId: string;
 }
 interface NewReportFormData {
-  clerkUserId: string;
   reportName: string;
   airScore: number;
   animalsScore: number;
@@ -93,18 +85,14 @@ export async function getUser(email: string) {
 export async function createUser(newUserFormData: NewUserFormData) {
   const {
     email,
-    password,
     firstName,
     lastName,
     clerkId,
   } = newUserFormData;
   try {
-    let salt = genSaltSync(10);
-    let hash = hashSync(password, salt);
   
     return await db.insert(UsersTable).values({
       emailAddress: email,
-      password: hash,
       firstName,
       lastName,
       clerkId,
