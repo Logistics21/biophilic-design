f1b0850ef9bdb4e5aa8de6ab8c11dddb624441fa
David Halsey <dhalsey212@gmail.com>
2024-06-04 09:54:16 -0400

    Add SignupButton to TopNav; delete old auth

--- app/_components/topnav.tsx
+++ app/_components/topnav.tsx
@@ -1,5 +1,8 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import leafIcon from "public/images/leaf.svg"
 
 export async function TopNav() {
     const user = await currentUser()
@@ -10,16 +13,31 @@ export async function TopNav() {
     }
 
     return (
-        <nav className="flex w-full items-center justify-end border-b p-4 text-xl font-semibold bg-black">
-            <div className="flex flex-row items-center gap-4 text-white">
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          {firstName}
-          <UserButton />
-        </SignedIn>
-      </div>
-        </nav>
+      <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold bg-white">
+        <div className="flex flow-row items-center gap-x-4">
+          <Image
+            src={leafIcon}
+            width={32}
+            height={32}
+            alt="a green leaf with little bubbles"
+          />
+          <h1>Biophilic Design</h1>
+        </div>
+        <div className="flex flex-row items-center gap-4 text-black">
+          <Link
+            href="/about"
+          >
+            Learn More
+          </Link>
+          <SignedOut>
+            <SignUpButton />
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            {firstName}
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
     )
 }
